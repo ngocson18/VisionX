5dbedb2b8e9c3e7c07580a33135235507d12b3c5
Ngoc Son <ngocsonxx98@gmail.com>
2023-07-24 17:25:12 +0700

    update ui

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -6,8 +6,7 @@ import { Component, OnInit  } from '@angular/core';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
-  isCollapsed = false;
-  listUserTest = [];
+  isCollapsed = true;
   constructor(
   ) {
     
